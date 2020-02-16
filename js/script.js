26223b9cbd199a16e09c528d3966f5e6781f38e6
13toxa21 <13toxa21@mail.ru>
2020-02-16 21:06:14 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document. getElementById("menu").onclick = function(){
+  addMenu();
+};
+
+function addMenu(){
+  document.getElementById("container1_hover").classList.toggle("show");
+};
+
+document. getElementById("close").onclick = function(){
+  addMenu();
+};
+
+function addMenu(){
+  document.getElementById("container1_hover").classList.toggle("show");
+};
+
